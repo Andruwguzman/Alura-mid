7dd32e8d25b994df9609dd4dcd2572d818fc4fd9
Andruwguzman <110174962+Andruwguzman@users.noreply.github.com>
2023-11-10 09:05:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
-function tocaSomPom(){document.queryselector('#som_tecla_pom').play('');
+function tocaSomPom(){document.queryselector('#som_tecla_pom').play('');}
+const elemento = document.queryselector(selectorAudio)
+
+
+const listaDeTeclas =document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++){
+
+
+const tecla = listaDeTeclas[contador]
+
+const instrumento = tecla.classList[1];
+
+const idAudio = '#som_${instrumento}';}
+
+tecla.onclick = function{
+    tocaSomPom (idAudio);
+};
+
+tecla.onekeydown = function(evento){
+    if(evento.code === 'space' || evento.code === 'Enter'){
+        tecla.ClassList.add('ativa');
+    }
+}
+tecla.onkeyup = function(evento){
+    tecla.classList.remove
 }
 document.querySelector('tecla_pom');
 document.queryselector('#som_tecla_pom').play('');
+document.queryselector('.tecla_pom').onclick = tocaSomPom
